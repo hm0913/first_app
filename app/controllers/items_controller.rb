f8e9f45b2ec8f96f92ca677a90becaedd72bdafd
hm0913 <75556338+hm0913@users.noreply.github.com>
2021-02-13 17:08:30 +0900

    商品の編集機能作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,6 +11,17 @@ class ItemsController < ApplicationController
     redirect_to root_path
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to root_path
+  end
+
+
 
   private
   def item_params
